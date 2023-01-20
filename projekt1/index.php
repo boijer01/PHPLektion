7d1fbd1b3230816133400a15a2e91b19479535aa
Simon <boijersi@arcada.fi>
2023-01-20 11:36:58 +0200

    Datum & tid

--- projekt1/index.php
+++ projekt1/index.php
@@ -52,6 +52,23 @@
                 <p>Elcyklar och solkraft</p>
             </article>
 
+            <article>
+                <h2>Uppgift 2 Datum & tid</h2>
+                <?php
+                $datum = date("d.M.Y");
+
+                print("Dagens datum är: <br>" .$datum);
+                //Arrays
+                $veckodagar = array("Måndag","Tisdag","Onsdag","Torsdag","Fredag","Lördag","Söndag");
+                print("<br>Idag är det veckodag nr: " .date("l"));
+                print("<p>Förrsöker igen, är det veckodag nr: " .date("m")." då");
+
+                print("<p>Veckodag nummer 01 betyder att det är " .$veckodagar[0] . "</p>")
+
+
+                ?>
+            </article>
+
         </section>
 
         <!-- Footern innehåller t.ex. somelänkar och kontaktuppg -->
