1eea006dc44224051192fe991e4507dc37dfa9d6
Simon <boijersi@arcada.fi>
2023-01-23 14:47:34 +0200

    Något med tid tror jag

--- projekt1/uppg3.php
+++ projekt1/uppg3.php
@@ -16,4 +16,17 @@
         //xss attack 
         print("<br>Your email address is: " . $_REQUEST["email"]);
     }
+
+    //Lite övningar med time() och mktime()
+    if(!empty($_REQUEST["name"])) {
+
+    $dag = test_input($_REQUEST["name"]);
+    print("<p>Du matade in dagen #" . $dag . "</p>");
+
+    $tidnu = time();
+    print("Just nu är det " . $tidnu . " sekunder sedan 1970-01-01 00:00:00 UTC");
+
+    $inmatadTid = mktime(12,01,0,1,$dag,2023);
+    print("<br>Du undrar ifall " . $inmatadTid . " är i framtiden eller i det förflutna");
+    }
 ?> 
