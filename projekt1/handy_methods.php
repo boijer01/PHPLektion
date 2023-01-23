ae645ff9b78c512820f9ddae92c9920f3fc51d1b
Simon <boijersi@arcada.fi>
2023-01-23 14:11:40 +0200

    Safety grejer på forms lektion 3

--- projekt1/handy_methods.php
+++ projekt1/handy_methods.php
@@ -2,10 +2,14 @@
 
 // allt möjlgt viktigt som vi använder ofta, sessionshantering, form validation, etc.
 
+
+//En funktion som tar bort whitespace, backslashes och gör om < till html safe motsvarigheter.
 function test_input($data) {
   $data = trim($data);
   $data = stripslashes($data);
   $data = htmlspecialchars($data);
   return $data;
 }
+
+$safe_input = test_input($_REQUEST["unsafedata"]);
 ?>
--- projekt1/uppg3.php
+++ projekt1/uppg3.php
@@ -6,7 +6,14 @@
     //$_REQUEST är en superglobal som innehåller både $_GET och $_POST
 
     if(!empty($_REQUEST["name"]) && isset($_REQUEST["email"])) {
-        print ("Welcome " . $_REQUEST["name"]);
+
+        //Såhär använder vi test_input funktionen
+        $skadligdata = $_REQUEST["name"];
+        $ofarligdata = test_input($skadligdata);
+
+        print ("Welcome " . $ofarligdata);
+
+        //xss attack 
         print("<br>Your email address is: " . $_REQUEST["email"]);
     }
 ?> 
