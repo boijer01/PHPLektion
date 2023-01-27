e549059882323e3792241213ae7c274405aef60a
Simon <boijersi@arcada.fi>
2023-01-27 11:35:40 +0200

    Lektion klar

--- projekt1/uppg1.php
+++ projekt1/uppg1.php
@@ -1,2 +1,6 @@
 <?php
-        print("<p>Servern snurrar på port " .$_SERVER['REMOTE_PORT'] . ", som vanligt med https och apache.");
+
+//Hälsa på återkommande användare
+print("Välkommen tillbaka ".$_SESSION['username']);
+
+print("<p>Servern snurrar på port " .$_SERVER['REMOTE_PORT'] . ", som vanligt med https och apache.");
--- projekt1/uppg4.php
+++ projekt1/uppg4.php
@@ -7,18 +7,26 @@ if (!empty($_REQUEST["name"]) && isset($_REQUEST["email"])) {
     $ofarligdata = test_input($skadligdata);
     $email = test_input($_REQUEST("email"));
 
+    $username = $ofarligdata;
+
     print("Welcome " . $ofarligdata);
 
     //xss attack 
     print("<br>Your email address is: " . $_REQUEST["email"]);
 }
 
+
+    //Spara användarnamnet i sessionsvariabel så att vi kan hälsa på återkommande användare
+    $_SESSION['username'] = $username;
+
     //Generera lösenord
-    /*$pass = "6346346k356k3o46";
+    $pass = "6346346k356k3o46";
     mail($email, "Your password for denApp", $pass);
     print("signing up as ".$ofarligdata. "  Check your email for password");
-    */
+    
     // Quick hack för att tömma get informationen, html meta refresh
+    
+
     
     
     //print("Du kmr att refreshas om 5sek");
