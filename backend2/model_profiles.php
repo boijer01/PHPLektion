d3dca754e1888ee652ed20ada457e100d974a325
boijersi <boijersi@arcada.fi>
2023-02-24 10:58:26 +0200

    PDO login broken men fixad men ändå ej

--- backend2/model_profiles.php
+++ backend2/model_profiles.php
@@ -1,12 +1,6 @@
 <?php
 $sql = "SELECT * FROM profiles";
-$stmt = $conn->query($sql); // query metoden av mysqli objektet returnerar mysqli_result objektet
+$stmt = $conn->query($sql);
+$result = $stmt->fetch(PDO::FETCH_ASSOC);
+print("Användarnamn: ". $row['username']);
 
-// mysqli_result objektet har en egenskap som heter num_rows
-if ($stmt->num_rows > 0) { 
-    while($row = $stmt->fetch_assoc()) {
-        print("Användarnamn: " . $row['username']);
-    }
-}
-
-// ToDo: Kör SQL kod på databasen
